e0c77f62ab8301e9af05de7d361e9ffe8b368f08
Blake Sager <blake.sager@activeprospect.com>
2018-07-02 20:47:07 -0500

    dink

--- 07_data-structures/02_Slice/04_increment/main.go
+++ 07_data-structures/02_Slice/04_increment/main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	mySlice := make([]int, 2)
-	fmt.Println(mySlice)
+	mySlice := make([]int, 1)
 	fmt.Println(mySlice[0])
 	mySlice[0] = 7
 	fmt.Println(mySlice[0])
